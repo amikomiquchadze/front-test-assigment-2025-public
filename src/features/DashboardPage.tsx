409bd7f9e10fcc31c85f233ddb898d860390ec5c
amikomiquchadze <amikomiquchadze1@gmail.com>
2025-11-28 01:54:32 -0500

    retrieve data via useCampaignData

---
+++ src/features/DashboardPage.tsx
@@ -0,0 +1,9 @@
+import { useCampaignData } from "../hooks/useCampaignData";
+
+function DashboardPage() {
+    const { loading, error, metadata, metrics } = useCampaignData();
+
+    return (
+    
+)
+}

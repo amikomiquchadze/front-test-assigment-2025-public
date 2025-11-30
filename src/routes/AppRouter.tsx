d053b2ae7105896b60b2c58c42de8ce62246338a
amikomiquchadze <amikomiquchadze1@gmail.com>
2025-11-30 18:41:28 -0500

    Set up Routing

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,20 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { DashboardPage } from "../features/dashboard/DashboardPage";
+function AppRouter() {
+  const isAuthenticated = true;
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          isAuthenticated ? <Navigate to="/dashboard" replace /> : <Navigate to="/login" replace />
+        }
+      />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+}
+
+export default AppRouter;

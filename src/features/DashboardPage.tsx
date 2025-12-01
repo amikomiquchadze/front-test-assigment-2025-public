4be530ca3f63057adb1049dd13039b04a1d19e81
amikomiquchadze <amikomiquchadze1@gmail.com>
2025-12-01 03:00:37 -0500

    feat: finalize dashboard

--- src/features/DashboardPage.tsx
+++
@@ -1,9 +0,0 @@
-import { useCampaignData } from "../hooks/useCampaignData";
-
-function DashboardPage() {
-    const { loading, error, metadata, metrics } = useCampaignData();
-
-    return (
-    
-)
-}

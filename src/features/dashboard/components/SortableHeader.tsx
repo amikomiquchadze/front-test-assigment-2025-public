5b237ff152046a6021fe1f8e76e3ec0a7126b719
amikomiquchadze <amikomiquchadze1@gmail.com>
2025-12-01 02:51:49 -0500

    feat: SortableHeader

---
+++ src/features/dashboard/components/SortableHeader.tsx
@@ -0,0 +1,35 @@
+import styles from "./DataTable/DataTable.module.css";
+
+type SortableHeaderProps = {
+  label: string;
+  active: boolean;
+  direction: SortDirection;
+  onClick: () => void;
+  align?: "left" | "right";
+};
+type SortDirection = "asc" | "desc";
+
+function SortableHeader({
+  label,
+  active,
+  direction,
+  onClick,
+  align = "left",
+}: SortableHeaderProps) {
+  return (
+    <th
+      onClick={onClick}
+      className={`${styles.thBase} ${align === "right" ? styles.right : ""} ${active ? styles.active : styles.inactive
+        }`}
+    >
+      <span className={styles.thContent}>
+        {label}
+        <span className={`${styles.arrow} ${active ? "active" : ""}`}>
+          {active ? (direction === "asc" ? "↑" : "↓") : "↕"}
+        </span>
+      </span>
+    </th>
+  )
+};
+
+export default SortableHeader;
